2bf320a4113f2e503279cb60f2dfbf2214425e46
Zharfan23 <zharfanpintar@gmail.com>
2025-03-12 13:08:54 +0700

    ganti route

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\KategoriBukuController;
 use App\Http\Controllers\BukuController;
-use App\Http\Controllers\Buku;
+use App\Http\Controllers\PetugasController;
+
 
 Route::get('/', function () {
     return view('welcome');
